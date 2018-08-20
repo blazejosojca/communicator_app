8dd9162e93adfdf53668bced694ce84b2cdab440
blazejosojca <blazejosojca@gmail.com>
2018-08-21 00:27:58 +0200

    user class methods changed after tests

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 from passlib.hash import argon2
+from database.db_production import connect_to_db, close_connection
 
 
 class User(object):
@@ -21,19 +22,19 @@ class User(object):
 
     def save_to_db(self, cursor):
         if self.__id == -1:
-
             sql = """INSERT INTO users(username, email, hashed_password) 
                 VALUES (%s, %s, %s)"""
-            values = (self.email, self.username, self.hashed_password)
+            values = (self.username, self.email, self.hashed_password)
+            print(values)
             cursor.execute(sql, values)
-            print(f"User {self.username} was added to DB")
             return True
         return False
 
 
     def verify_user(self, cursor, username, password_try):
-        sql = f"SELECT * FROM users WHERE username={username} LIMIT 1"
-        cursor.execute(sql)
+        sql = "SELECT * FROM users WHERE username=%s LIMIT 1"
+        value = (username, )
+        cursor.execute(sql, value)
         data = cursor.fetchone()
         if data is not None:
             self.__id = data[0]
@@ -52,8 +53,9 @@ class User(object):
 
     @staticmethod
     def load_by_user_id(cursor, user_id):
-        sql = f'SELECT * FROM users WHERE id={user_id}'
-        cursor.execute(sql)
+        sql = 'SELECT * FROM users WHERE id= %s'
+        value = (user_id,)
+        cursor.execute(sql, value)
         data = cursor.fetchone()
 
         if data is not None:
@@ -63,8 +65,7 @@ class User(object):
             loaded_user.email = data[2]
             loaded_user.__hashed_password = data[3]
             return loaded_user
-        else:
-            return None
+        return None
 
     @staticmethod
     def load_all_users(cursor):
@@ -82,14 +83,39 @@ class User(object):
             all_users.append(loaded_user)
         return all_users
 
-    def delete(self, cursor):
+    def delete_user(self, cursor):
         sql = "DELETE FROM users WHERE id=%s"
-        cursor.execute(sql, self.__id)
-        self.__id = -1
+        value = (self.__id,)
+        cursor.execute(sql, value)
+        # self.__id = -1
         return True
 
     def __repr__(self):
-        pass
+        return f'{self.user_id} {self.username} - {self.email}'
 
     def __str__(self):
-        return  'User: {}, id: {}, email:{}'.format(self.username, self.__id, self.email)
+        return  'User: {}, id: {}, email:{}'.\
+            format(self.username, self.__id, self.email)
+
+
+cnx, cursor = connect_to_db()
+
+# new_user3 = User()
+# # #new_user.username = 'grazyna'
+# # # # new_user.email = "grazyna@mail.com"
+# # # # new_user.set_password('haslo')
+# # # # new_user.save_to_db(cursor)
+# # new_user3.username = 'buu'
+# # new_user3.email = "buuu@mail.com"
+# # new_user3.set_password('buuu')
+# # new_user3.save_to_db(cursor)
+# # close_connection(cursor, cnx)
+# # user_to_delete = User.load_by_user_id(cursor, 12)
+# # print(user_to_delete)
+# # user_to_delete.delete_user(cursor)
+# all = User.load_all_users(cursor)
+# print(all)
+# # all = User.load_all_users(cursor)
+# # print(all)
+# # new_user3.delete_user(cursor, 2)
+# close_connection(cursor, cnx)
