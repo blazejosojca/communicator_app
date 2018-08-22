9367f8a1093a7fa19051819e2b57e16874c9dfd9
blazejosojca <blazejosojca@gmail.com>
2018-08-22 23:14:24 +0200

    added class method for setting value of creation_date - removed this value form init - cleaner code

--- models/message.py
+++ models/message.py
@@ -1,4 +1,4 @@
-import time
+from time import strftime
 from models.user import User
 from database.db_production import connect_to_db, close_connection
 
@@ -10,17 +10,21 @@ class Message:
         self.from_user_id = ""
         self.to_user_id = ""
         self.text = ""
-        self.creation_date = time.strftime('%Y-%m-%d')
+        self.creation_date = ''
 
     @property
     def message_id(self):
         return self.__id
 
+    @classmethod
+    def set_creation_date(cls):
+        return strftime('%Y-%m-%d')
+
     def save_message_to_db(self, cursor):
         if self.__id == -1:
             sql = """INSERT INTO messages(from_user_id, to_user_id, text, creation_date)
                         VALUES (%s, %s, %s, %s)"""
-            values = (self.from_user_id, self.to_user_id, self.text, self.creation_date)
+            values = (self.from_user_id, self.to_user_id, self.text, Message.set_creation_date())
             cursor.execute(sql, values)
             return True
         return False
@@ -58,8 +62,23 @@ class Message:
             all_mssgs.append(loaded_message.__str__())
         return all_mssgs
 
-    def load_all_messages_for_user(self, cursor):
-        pass
+    # this method load message for user by from_user_id
+    @staticmethod
+    def load_all_messages_for_user(cursor, user_id):
+        sql = 'SELECT * FROM messages WHERE from_user_id = %s'
+        value = (user_id,)
+        cursor.execute(sql, value)
+        data = cursor.fetchone()
+
+        if data is not None:
+            loaded_mssg = Message()
+            loaded_mssg.__id = data[0]
+            loaded_mssg.from_user_id = data[1]
+            loaded_mssg.to_user_id = data[2]
+            loaded_mssg.text = data[3]
+            loaded_mssg.creation_date = data[4]
+            return loaded_mssg.__str__()
+        return None
 
     def __str__(self):
-        return f'from {self.from_user_id} to {self.to_user_id}: {self.text} '
+        return f'from {self.from_user_id} to {self.to_user_id}: {self.text}'
