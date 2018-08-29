cc4634f16a692f1f57886cbac8324d17c37feaf8
blazejosojca <blazejosojca@gmail.com>
2018-08-30 01:00:59 +0200

    cleared code - prepared for right parsers ;-)

--- arg_test.py
+++
@@ -1,4 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description="""This is communicator_app.
-                                             Application which allows to send and receive messages.'
-                                             """)
--- message_manager.py
+++ message_manager.py
@@ -1,20 +1,3 @@
 import argparse
 
-
-def manage_options():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--username", action="store",
-                        dest="username", help="User login")
-    parser.add_argument("-p", "--password", action="store",
-                        dest="password", help="User password")
-    parser.add_argument("-l", "--list", action="store_true",
-                        dest="list", help="Get message list")
-    parser.add_argument("-s", "--send", action="store",
-                        dest="delete", help="Send message")
-    parser.add_argument("-t", "--to", action="store",
-                        dest="modify", help="Message for")
-
-    parse_options = parser.parse_args()
-    return parse_options
-
-# TODO - fix this stuff
+# TODO - fix this
