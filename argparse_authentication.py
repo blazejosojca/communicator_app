0bb05a71fa7f05c1574d31a85c7c6724358827c2
blazejosojca <blazejosojca@gmail.com>
2018-09-01 23:43:07 +0200

    deleted useless directories

--- argparse_authentication.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description="Authentication module",add_help=True)
-
-group = parser.add_argument_group('authentication')
-
-group.add_argument('-u', '--user', dest="username", action="store", help="User name")
-group.add_argument('-p', '--password', dest="password", action="store", help="User password")
--- message_manager.py
+++
@@ -1,3 +0,0 @@
-import argparse
-
-# TODO - fix this
